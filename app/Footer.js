b4b25ab6c6d9eecb97edf64b926221007d187437
dev gurjar <125337294+devgurjar-coder@users.noreply.github.com>
2024-04-19 10:10:06 +0530

    Update Footer.js

--- app/Footer.js
+++ app/Footer.js
@@ -9,15 +9,17 @@ const Footer = () => {
         <a href="https://www.linkedin.com/in/dev-gurjar63" target="_blank">
           <img src="linkdin.png" alt="LinkedIn" />
         </a>
-        <a href="https://github.com/devgurjar-coder" target="_blank">
-          <img src="github.png" alt="GitHub" />
-        </a>
         <a href="https://twitter.com/@devgurjar_63" target="_blank">
           <img src="twitter.png" alt="Twitter" />
         </a>
-        <a href="https://mail.google.com" target="_blank">
-          <img src="Gmail.webp" alt="Gmail" />
+        <a href="https://www.instagram.com/editshifters?utm_source=qr&igsh=c3Q1bWo5YTJwd" target="_blank">
+          <img src="instagram.avif" alt="instagram" />
         </a>
+       
+        <a href="mailto:gurjarsunny420@gmail.com" target="_blank">
+  <img src="Gmail.webp" alt="Gmail" />
+</a>
+
       </div>
       <div className="number">
         <BiPhoneCall />

e14233f60c0bc65f2542d9f113085ff1a484ac87
dev gurjar <125337294+devgurjar-coder@users.noreply.github.com>
2024-05-05 17:14:55 +0530

    Update Header.js

--- app/Header.js
+++ app/Header.js
@@ -3,23 +3,30 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className="main">
+    <header className="main">
       <div className="left">
-        <h2>Hello</h2>
-        <h3>Dev Here</h3>
-        <h1>
-          <span>SHORT FORM</span> Video Editor
-        </h1>
-        <p>Your go-to freelace short-form video edior! I may be new to the scene, but my creativity knows no bounds. I specialize in crafting engaging video that leaves a lasting impression Let's collaborate and turn your vision into captivating content that shines!</p>
+        <h2>Hello,</h2>
+        <h3>I'm a Dev Gurjar</h3>
+        {/* <h1 className="hero-text">
+          <span>Short-Form</span> Video Editor
+        </h1> */}
+        <p>
+          Your go-to freelance short-form video editor! I may be new to the scene,
+          but my creativity knows no bounds. I specialize in crafting engaging
+          videos that leave a lasting impression. Let's collaborate and turn your
+          vision into captivating content that shines!
+        </p>
         <a href="https://www.upwork.com/freelancers/~01434f2094a86cefc9" target="_blank">
-  <button>Hire Me</button>
-</a>
-
+          <button>Hire Me</button>
+        </a>
+        <a href="mailto:gurjarsunny420@gmail.com" target="_blank">
+          <button>Email Me</button>
+        </a>
       </div>
       <div className="right">
-        <img src="mes.png" alt="" />
+        <img src="profile-picture.jpg" alt="Your Profile Picture" />
       </div>
-    </div>
+    </header>
   );
 };
 

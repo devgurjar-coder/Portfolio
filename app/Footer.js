78dac95b61f6a24923837e175582412c484d458f
dev gurjar <125337294+devgurjar-coder@users.noreply.github.com>
2024-05-07 10:37:19 +0530

    Update Footer.js

--- app/Footer.js
+++ app/Footer.js
@@ -8,12 +8,12 @@ const Footer = () => {
       <div className="links-container"> {/* Corrected class name */}
         <a href="https://www.linkedin.com/in/dev-gurjar63" target="_blank">
           <img src="linkdin.png" alt="LinkedIn" />
-        </a>  
+        </a>
         <a href="https://twitter.com/@devgurjar_63" target="_blank">
           <img src="twitter.png" alt="Twitter" />
         </a>
         <a href="https://www.instagram.com/editshifters?utm_source=qr&igsh=c3Q1bWo5YTJwd" target="_blank">
-          <img src="instagram-7411557_960_720.webp" alt="" />
+          <img src="transparent-instagram-logo-icon-5ef5b92a53cea4.0701610815931620263433.jpg" alt="" />
         </a>
         <a href="mailto:gurjarsunny420@gmail.com" target="_blank">
           <img src="Gmail.webp" alt="Gmail" />
